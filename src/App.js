6239ae47c6b39964f5c422a97a76bf9816dcef26
JuliaPark <pje106@gmail.com>
2022-12-19 11:28:34 -0800

    connected API for patch and delete

--- src/App.js
+++ src/App.js
@@ -1,47 +1,75 @@
 import React from 'react';
 import TaskList from './components/TaskList.js';
 import './App.css';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 
-const INITIAL_TASKS = [
-  {
-    id: 1,
-    title: 'Mow the lawn',
-    isComplete: false,
-  },
-  {
-    id: 2,
-    title: 'Cook Pasta',
-    isComplete: true,
-  },
-];
+// const INITIAL_TASKS = [
+//   {
+//     id: 1,
+//     title: 'Mow the lawn',
+//     isComplete: false,
+//   },
+//   {
+//     id: 2,
+//     title: 'Cook Pasta',
+//     isComplete: true,
+//   },
+// ];
 
 const App = () => {
-  const initialCopy = INITIAL_TASKS.map((task) => {
-    return { ...task };
-  });
-
-  const [taskList, setTaskList] = useState(initialCopy);
+  // const initialCopy = INITIAL_TASKS.map((task) => {
+  //   return { ...task };
+  // });
+  const [taskList, setTaskList] = useState([]);
+  const URL = 'https://task-list-api-c17.herokuapp.com/tasks';
+  useEffect(() => {
+    axios
+      .get(URL)
+      .then((res) => {
+        console.log(res);
+        const tasksAPIResCopy = res.data.map((task) => {
+          return {
+            id: task.id,
+            title: task.title,
+            isComplete: task.is_complete,
+          };
+        });
+        setTaskList(tasksAPIResCopy);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
 
   const updateComplete = (taskId, taskIsComplete) => {
     console.log('in updateComplete');
     const newTaskList = [];
-    for (const task of taskList) {
-      if (task.id !== taskId) {
-        newTaskList.push(task);
-      } else {
-        const newTask = {
-          ...task,
-          isComplete: taskIsComplete,
-        };
-        newTaskList.push(newTask);
-      }
-    }
-    setTaskList(newTaskList);
+    axios
+      .patch(`${URL}/${taskId}/mark_complete`)
+      .then(() => {
+        for (const task of taskList) {
+          if (task.id !== taskId) {
+            newTaskList.push(task);
+          } else {
+            const newTask = {
+              ...task,
+              isComplete: taskIsComplete,
+            };
+            newTaskList.push(newTask);
+          }
+        }
+        setTaskList(newTaskList);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
   };
-
   const deleteTask = (taskId) => {
     console.log('deleteTask Called');
+    axios
+    .delete(`${URL}/${taskId}`)
+    .then(() => {
     const newTaskList = [];
     for (const task of taskList) {
       if (task.id !== taskId) {
@@ -49,6 +77,10 @@ const App = () => {
       }
     }
     setTaskList(newTaskList);
+  })
+    .catch((err) => {
+      console.log(err);
+    });
   };
 
   return (
@@ -58,7 +90,11 @@ const App = () => {
       </header>
       <main>
         <div>
-          <TaskList deleteTask={deleteTask} updateComplete={updateComplete} tasks={taskList} />
+          <TaskList
+            deleteTask={deleteTask}
+            updateComplete={updateComplete}
+            tasks={taskList}
+          />
         </div>
       </main>
     </div>
